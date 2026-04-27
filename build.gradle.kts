d1c6a4ae926425e5fd62f3b67bed2cde04341689
Glavo <zjx001202@gmail.com>
2026-04-27 09:17:45 +0800

    Add compileOnly dependencies, switch to java-library plugin, and set Java 11

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("java")
+    id("java-library")
 }
 
 group = "org.glavo"
@@ -10,9 +10,16 @@ repositories {
 }
 
 dependencies {
+    compileOnly("org.jetbrains:annotations:26.1.0")
+    compileOnly("com.ibm.icu:icu4j:78.3")
+
     testImplementation(platform("org.junit:junit-bom:6.0.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType(JavaCompile::class) {
+    options.release.set(11)
 }
 
 tasks.test {
