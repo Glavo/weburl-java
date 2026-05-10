669b6528cbc5184feda59b3558216b7c1b05cbdb
Glavo <zjx001202@gmail.com>
2026-05-10 18:13:49 +0800

    Release 0.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 }
 
 group = "org.glavo"
-version = "0.1.0" + "-SNAPSHOT"
+version = "0.1.0" // + "-SNAPSHOT"
 description = "A modern Java URL library that brings browser-grade WHATWG URL parsing to URI-style APIs."
 
 repositories {
